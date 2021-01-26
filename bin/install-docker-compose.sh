821a86041e19ce4b2638166d46061144723383bf
Andrius Kairiukstis <k@c0.lt>
2021-01-26 19:01:19 +0100

    docker-compose installer

--- bin/install-docker-compose.sh
+++ bin/install-docker-compose.sh
@@ -6,8 +6,8 @@ set -e
 LATEST_URL="$(curl -Ls -o /dev/null -w '%{url_effective}' https://github.com/docker/compose/releases/latest)"
 COMPOSE_VERSION="${LATEST_URL##*/}"
 DOWNLOAD_URL="https://github.com/docker/compose/releases/download/${COMPOSE_VERSION}/docker-compose-$(uname -s)-$(uname -m)"
-curl -L "${DOWNLOAD_URL}" -o /usr/local/bin/docker-compose
-chmod +x /usr/local/bin/docker-compose
+sudo curl -L "${DOWNLOAD_URL}" -o /usr/local/bin/docker-compose
+sudo chmod +x /usr/local/bin/docker-compose
 set +e
 docker-compose -v > /dev/null 2>&1 || echo "There is issues with docker-compose, please check!"
 set -e
