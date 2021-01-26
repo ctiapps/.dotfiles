f7d73639e94d6e8260bd6343ecbbfddd02254a43
Andrius Kairiukstis <k@c0.lt>
2021-01-26 19:00:17 +0100

    docker-compose installer

---
+++ bin/install-docker-compose.sh
@@ -0,0 +1,14 @@
+#!/usr/bin/env bash
+# vim: set tabstop=4 softtabstop=4 shiftwidth=4 expandtab smarttab filetype=sh fileencoding=utf-8 :
+set -ueox pipefail
+
+set -e
+LATEST_URL="$(curl -Ls -o /dev/null -w '%{url_effective}' https://github.com/docker/compose/releases/latest)"
+COMPOSE_VERSION="${LATEST_URL##*/}"
+DOWNLOAD_URL="https://github.com/docker/compose/releases/download/${COMPOSE_VERSION}/docker-compose-$(uname -s)-$(uname -m)"
+curl -L "${DOWNLOAD_URL}" -o /usr/local/bin/docker-compose
+chmod +x /usr/local/bin/docker-compose
+set +e
+docker-compose -v > /dev/null 2>&1 || echo "There is issues with docker-compose, please check!"
+set -e
+
